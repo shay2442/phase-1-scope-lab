fbcc0df56b26a24de1cba9076a38688f0ac2f5ce
Shay Wacker <shay2442@yahoo.com>
2021-08-14 15:00:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    console.log(customerName)
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+var bestCustomer 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+const leastFavoriteCustomer = 'Shay'
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'shay'
+
+}
+
+function overwriteBestCustomer(name) {
+    bestCustomer = name 
+
+}
+
